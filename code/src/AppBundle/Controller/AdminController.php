b43798916eca93f4b41716f9515b601c1238e1b8
Colin Silcock <colin@linx.net>
2016-08-29 19:16:13 +0100

    Add simple guests summary in admin area

---
+++ code/src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: colin
+ * Date: 29/08/2016
+ * Time: 18:47
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin/guests")
+     */
+    public function guests() {
+        $year = date('Y');
+        $guestRepository = $this->getDoctrine()->getRepository('AppBundle:Guest');
+        $guests = $guestRepository->findAll();
+        return $this->render(
+            'admin/guests.html.twig',
+            array('year' => $year,
+                'guests' => $guests
+            )
+        );
+    }
+
+}
