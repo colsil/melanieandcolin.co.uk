3af96ee57f060b455cc8f3ce7677d03f6711d819
Colin Silcock <colin@linx.net>
2016-08-29 18:16:36 +0100

    Improve deployment mechanism and changed deployment script to reflect upgrade to php 7 on production host

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,7 +4,8 @@ set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&  [ "${TRAVIS_PHP_VERSION}" = "5.6" ]  && [ "${TRAVIS_BRANCH}" = "master" ]; then
     tar -czf site.tar.gz code/*
     scp -i build/sshkeys/travis_key.rsa site.tar.gz ubuntu@melanieandcolin.uk:
-    ssh -i build/sshkeys/travis_key.rsa ubuntu@melanieandcolin.uk "tar -xzf site.tar.gz -C /var/www/melanieandcolin.co.uk"
+    ssh -i build/sshkeys/travis_key.rsa ubuntu@melanieandcolin.uk "rm -rf /tmp/melanieandcolin.co.uk && mkdir /tmp/melanieandcolin.co.uk && tar -xzf site.tar.gz -C /tmp/melanieandcolin.co.uk"
+    ssh -i build/sshkeys/travis_key.rsa ubuntu@melanieandcolin.uk "rsync -azv --exclude "parameters.yml" --exclude "cache" --exclude "sessions" --delete /tmp/melanieandcolin.co.uk/ /var/www/melanieandcolin.co.uk/
     ssh -i build/sshkeys/travis_key.rsa ubuntu@melanieandcolin.uk "php /var/www/melanieandcolin.co.uk/code/bin/console doctrine:migrations:migrate --no-interaction"
     ssh -i build/sshkeys/travis_key.rsa ubuntu@melanieandcolin.uk "php /var/www/melanieandcolin.co.uk/code/bin/console cache:clear --env=prod"
 fi
