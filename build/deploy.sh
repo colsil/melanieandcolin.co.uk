58c08df53fd905b7426297d8b14156d40dcaa6a2
Colin Silcock <colin@linx.net>
2016-08-29 18:17:05 +0100

    Improve deployment mechanism and changed deployment script to reflect upgrade to php 7 on production host

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 # deploy to production if php version is 5.6 and branch is master, and it's not a pull request!
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&  [ "${TRAVIS_PHP_VERSION}" = "5.6" ]  && [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&  [ "${TRAVIS_PHP_VERSION}" = "7.0" ]  && [ "${TRAVIS_BRANCH}" = "master" ]; then
     tar -czf site.tar.gz code/*
     scp -i build/sshkeys/travis_key.rsa site.tar.gz ubuntu@melanieandcolin.uk:
     ssh -i build/sshkeys/travis_key.rsa ubuntu@melanieandcolin.uk "rm -rf /tmp/melanieandcolin.co.uk && mkdir /tmp/melanieandcolin.co.uk && tar -xzf site.tar.gz -C /tmp/melanieandcolin.co.uk"
