026784648434e49dbf1486fc333ffb451cc342d9
Colin Silcock <colin@linx.net>
2016-08-31 23:24:24 +0100

    Add ability to add guests via the guests admin page

--- code/src/AppBundle/Controller/AdminController.php
+++ code/src/AppBundle/Controller/AdminController.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Guest;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -15,20 +16,70 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\RegistrationType;
 
 class AdminController extends Controller
 {
+    private function getRandomPassword(
+        $length,
+        $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    {
+        $str = '';
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        if ($max < 1) {
+            throw new Exception('$keyspace must be at least two characters long');
+        }
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+        return $str;
+    }
+
     /**
      * @Route("/admin/guests", name="guests")
      */
-    public function guests()
+    public function guests(Request $request)
     {
         $guestRepository = $this->getDoctrine()->getRepository('AppBundle:Guest');
         $guests = $guestRepository->findAll();
+
+        $numInvitedDay = count($guestRepository->findBy(['invitedday' => 1]));
+        $numAttendingDay = count($guestRepository->findBy(['attendingday' => 1]));
+        $numInvitedEvening = count($guestRepository->findBy(['invitedevening' => 1]));
+        $numAttendingEvening = count($guestRepository->findBy(['attendingevening' => 1]));
+
+        $form = $this->createForm(RegistrationType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $guest = new Guest();
+            $guestdata = $form->getData();
+            $guest->setEmail($guestdata['email'])
+                ->setName($guestdata['name'])
+                ->setSurname($guestdata['surname'])
+                ->setAttendingday($guestdata['attendingday'])
+                ->setInvitedday($guestdata['invitedday'])
+                ->setAttendingevening($guestdata['attendingevening'])
+                ->setInvitedevening($guestdata['invitedevening'])
+                ->setUsername($guestdata['email']);
+            $generatedPassword = $this->getRandomPassword(12);
+            $guest->setPlainPassword($generatedPassword);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($guest);
+            $em->flush();
+            return $this->redirectToRoute('guests');
+        }
+
         return $this->render(
             'admin/guests.html.twig',
             array(
-                'guests' => $guests
+                'guests' => $guests,
+                'form' => $form->createView(),
+                'numInvitedDay' => $numInvitedDay,
+                'numAttendingDay' => $numAttendingDay,
+                'numInvitedEvening' => $numInvitedEvening,
+                'numAttendingEvening' => $numAttendingEvening
             )
         );
     }
--- code/src/AppBundle/Form/RegistrationType.php
+++ code/src/AppBundle/Form/RegistrationType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Util\LegacyFormHelper;
 
@@ -12,37 +13,31 @@ class RegistrationType extends AbstractType
     {
         $builder->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'),
             array(
-                'label' => 'form.email',
+                'label' => false,
                 'attr' => array('class' => 'form-control'),
                 'translation_domain' => 'FOSUserBundle'
             )
         );
-        $builder->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
-            'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
-            'options' => array('translation_domain' => 'FOSUserBundle', 'attr' => array('class' => 'form-control')),
-            'first_options' => array('label' => 'form.password'),
-            'second_options' => array('label' => 'form.password_confirmation'),
-            'invalid_message' => 'fos_user.password.mismatch',
-        ));
-        $builder->add('username', null, array(
-                'label' => 'form.username',
-                'attr' => array('class' => 'form-control'),
-                'translation_domain' => 'FOSUserBundle'
-            )
-        );
-        $builder->add('name', null, array('label' => 'First Name', 'attr' => array('class' => 'form-control')));
-        $builder->add('surname', null, array('attr' => array('class' => 'form-control')));
-        $builder->add('invitedday', null, array('label' => 'Invited for Day', 'attr' => array('class' => 'form-control')));
-        $builder->add('invitedevening', null, array('label' => 'Invited for Evening', 'attr' => array('class' => 'form-control')));
+//        $builder->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
+//            'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+//            'options' => array('translation_domain' => 'FOSUserBundle', 'attr' => array('class' => 'form-control')),
+//            'first_options' => array('label' => 'form.password'),
+//            'second_options' => array('label' => 'form.password_confirmation'),
+//            'invalid_message' => 'fos_user.password.mismatch',
+//        ));
+//        $builder->add('username', null, array(
+//                'label' => false,
+//                'attr' => array('class' => 'form-control'),
+//                'translation_domain' => 'FOSUserBundle'
+//            )
+//        );
+        $builder->add('name', null, array('label' => false, 'attr' => array('class' => 'form-control')));
+        $builder->add('surname', null, array('label' => false, 'attr' => array('class' => 'form-control')));
+        $builder->add('invitedday', CheckboxType::class, array('label' => false, 'required' => false, 'attr' => array('class' => 'form-control')));
+        $builder->add('invitedevening', CheckboxType::class, array('label' => false, 'required' => false, 'attr' => array('class' => 'form-control')));
+        $builder->add('attendingevening', CheckboxType::class, array('label' => false, 'required' => false, 'attr' => array('class' => 'form-control')));
+        $builder->add('attendingday', CheckboxType::class, array('label' => false, 'required' => false, 'attr' => array('class' => 'form-control')));
     }
 
-    public function getParent()
-    {
-        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
-    }
 
-    public function getBlockPrefix()
-    {
-        return 'app_user_registration';
-    }
 }
