9a66de41926e3022971e28fbf712675c45ebf78c
Colin Silcock <colsil@users.noreply.github.com>
2017-06-10 11:27:14 +0100

    Upgrade user bundle to stable version. Improve cache script to fix issues.

--- code/.ebextensions/bin/update-cache.sh
+++ code/.ebextensions/bin/update-cache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in $(grep -l -R "ondeck" /var/app/ondeck/code/app/cache/prod/*); do
+for i in $(grep -l -R "ondeck" /var/app/ondeck/app/cache/prod/*); do
     sed -i -e "s/\/var\/app\/ondeck/\/var\/app\/current/g" $i
 done
