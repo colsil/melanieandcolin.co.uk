dcbc8fd50ab0acfc80814474ca51d7e9fb98c3a7
Colin Silcock <colsil@users.noreply.github.com>
2017-07-12 21:03:15 +0100

    Update with final timings of  the day

--- code/src/AppBundle/Controller/WelcomeController.php
+++ code/src/AppBundle/Controller/WelcomeController.php
@@ -68,14 +68,17 @@ class WelcomeController extends Controller
         $timings = [
             '12:00 - 12:45' => 'Guests Arrive',
             '13:00 - 13:30' => 'Ceremony',
-            '13:30 - 14:00' => 'Reception Drinks',
-            '14:30' => 'Canapés Served',
-            '16:00 - 16:30' => 'Wedding Breakfast Starts',
+            '13:30 - 15:15' => 'Reception Drinks & Canapés Served',
+            '15:15 - 15:45' => 'Speeches',
+            '15:45 - 16:00' => 'Guests to be seated in Marquee',
+            '16:00 - 18:00' => 'Wedding Breakfast Served',
             '19:00 - 19:30' => 'Evening Guests Arrive',
-            '20:00' => 'Cake Cutting & First Dance',
+            '19:30' => 'Cutting of the Cake',
+            '20:00' => 'First Dance',
             '20:00 - 21:00' => 'Band First Set',
-            '21:00 - 22:00' => 'Evening Buffet Served',
+            '20:30 - 22:30' => 'Evening Buffet Served',
             '22:00 - 23:00' => 'Band Second Set',
+            '00:00' => 'Music Finishes',
             '01:00' => 'Bar Closes'
         ];
 
